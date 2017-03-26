e1c5aab0ce1234344eb65c24c2fe006f8b6336c2
justnick <findnick@163.com>
2017-03-26 23:55:42 +0800

    initial commit

---
+++ spec/coeditor-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import Coeditor from '../lib/coeditor';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Coeditor', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('coeditor');
+  });
+
+  describe('when the coeditor:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.coeditor')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'coeditor:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.coeditor')).toExist();
+
+        let coeditorElement = workspaceElement.querySelector('.coeditor');
+        expect(coeditorElement).toExist();
+
+        let coeditorPanel = atom.workspace.panelForItem(coeditorElement);
+        expect(coeditorPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'coeditor:toggle');
+        expect(coeditorPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.coeditor')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'coeditor:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let coeditorElement = workspaceElement.querySelector('.coeditor');
+        expect(coeditorElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'coeditor:toggle');
+        expect(coeditorElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/coeditor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CoeditorView from '../lib/coeditor-view';
+
+describe('CoeditorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
