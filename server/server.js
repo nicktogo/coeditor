978f5ebaefb22aeca491745427327ed28b815743
justnick <findnick@163.com>
2017-04-13 22:30:10 +0800

    add broadcast(not tested)

--- server/server.js
+++ server/server.js
@@ -33,17 +33,7 @@ function startServer() {
         } else {
           // meta changes: cursor position, text selection
           // and open/save/close file
-          // TODO: broadcast changes to all clients by send method.
-          let sockets = allConnections[ws.sessionId];
-          sockets.forEach( (socket) => {
-            console.log(socket.getId());
-            console.log(ws.getId());
-            if (socket.getId() !== ws.getId()) {
-              if (socket) {
-                socket.send(msg);
-              }
-            }
-          });
+          broadcastMsg(msg, ws);
         }
       } else {
         // OT
@@ -57,6 +47,13 @@ function startServer() {
         allConnections[ws.sessionId].splice(index, 1);
         console.log('We just lost one connection: ' + ws.getId() + ' from ' + ws.sessionId);
         console.log('Now ' + ws.sessionId + ' has ' + allConnections[ws.sessionId].length + ' connection(s)');
+        // TODO send closed client id to other clients
+        let msg = {
+          a: 'meta',
+          type: 'close',
+          clientId: ws.getId()
+        };
+        broadcastMsg(msg, ws);
       }
     });
 
@@ -65,6 +62,17 @@ function startServer() {
   });
 
 };
+
+function broadcastMsg(msg, ws) {
+  let sockets = allConnections[ws.sessionId];
+  sockets.forEach( (socket) => {
+    console.log(socket.getId());
+    console.log(ws.getId());
+    if (socket && (socket.getId() !== ws.getId())) {
+      socket.send(msg);
+    }
+  });
+}
 
 WebSocket.prototype.createSession = function(data) {
   let sessionId = data.sessionId;
